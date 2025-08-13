5720187b53df05a8c6def83c3ad01035059494a2
Артем <artikus.sol@gmail.com>
2025-08-13 16:41:55 +0400

    fix(acai 1.0.0): интегрция виджета через пакет

--- templates/public/chat.php
+++ templates/public/chat.php
@@ -1,50 +1,21 @@
-<div class="chatbox">
+<div class="chatbox" id="chat-container">
+	<button class="chatbox__toggle">
+		<span class="chatbox__toggle-avatar"></span>
+	</button>
+
 	<div class="chatbox__wrapper">
 		<div class="chatbox__header">
-			<div class="chatbox__header-inner">
-				<button class="chatbox__close" type="button" tabindex="0" aria-label="Close chat">
-					<span class="chatbox__close-icon"></span>
-				</button>
-			</div>
+			<span></span>
+			<button class="chatbox__close"></button>
 		</div>
-
-		<div class="chatbox__body">
-			<!-- Messages -->
-			<div id="chatbox-messages" class="chatbox__message">
-				<div class="chatbox__messages-inner"></div>
-			</div>
-
-			<div class="chatbox__input-wrapper">
-				<div class="chatbox__input-container">
-					<form name="form_message" action="#" method="post" class="chatbox__input-form">
-		          <textarea
-			          name="message"
-			          cols="20"
-			          rows="1"
-			          dir="auto"
-			          autocapitalize="off"
-			          autocomplete="off"
-			          aria-controls="chatbox-messages"
-			          tabindex="0"
-			          class="chatbox__textarea"
-			          placeholder="Составьте свое сообщение..."
-		          ></textarea>
-						<button class="chatbox__send-button" tabindex="0" aria-label="Send">
-							<span class="chatbox__send-icon"></span>
-						</button>
-					</form>
-
-				</div>
-			</div>
-
-		</div>
+		<div class="chatbox__messages-inner"></div>
+		<form class="chatbox__input-form">
+			<textarea class="chatbox__textarea" placeholder="Напишите сообщение..." rows="1"></textarea>
+			<button type="submit" class="chatbox__send-button" tabindex="0" aria-label="Send">
+				<span class="chatbox__send-icon"></span>
+			</button>
 	</div>
 
-	<button class="chatbox__toggle" type="button">
-		<span class="chatbox__toggle-avatar">
-
-		</span>
-	</button>
 </div>
 
 
